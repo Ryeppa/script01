958dfa20612cdcd387296e704081d4f836056af7
Ryeppa <61437473+Ryeppa@users.noreply.github.com>
2020-03-15 20:18:59 +0700

    Add files via upload

---
+++ firepass.py
@@ -0,0 +1,16 @@
+print ("apa kabar Hxrs")
+print ("silahkan masukkan identitasmu")
+
+#mengambil input
+nama = raw_input ("Namamu: ")
+umur = input ("Usiamu: ")
+tinggi = input ("Tinggi Badan: ")
+alamat = raw_input ("Dimana Tempat Tinggalmu: ")
+
+#menapilkan input
+print "halo",nama
+print "berusia",umur
+print "tinggi tubuh",tinggi,"cm"
+print "tinggal di",alamat
+print "Selamat datang disystem kami"
+print "selamat mencoba"
